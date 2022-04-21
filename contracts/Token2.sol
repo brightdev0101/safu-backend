d8c8240fae64559b930375bd75c369f488c14f95
chrlschwb <chrlschwb77@outlook.com>
2022-04-20 18:52:20 -0800

    v3

--- contracts/Token2.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: Unlicensed
-pragma solidity >=0.7.0;
-
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
-
-abstract contract Token is ERC20 {}
--- routes/api.js
+++ routes/api.js
@@ -17,47 +17,71 @@ router.get("/getAll",(req,res)=>{
 });
   
 router.get("/test",(req,res)=>{
+
+    // res.json(req.body.userAddress);
+    const tokenName = "saf";
+    const tokenSymbol = "SF";
+    const tokenDecimal = 18;
+    const tokenSupply = 10002200;
+    const uniSwapRouter = "0xD99D1c33F9fC3444f8101754aBC46c52416550D1";
+
     data = compiler('token');
-    address2 = deployer(data);
-    res.json(address2);
+    (async () => {
+        address2 = await deployer(data, tokenName, tokenSymbol, tokenDecimal, tokenSupply, uniSwapRouter);
+        res.json(address2);
+    })()
+    
 });
 
 router.post("/createToken", (req,res)=>{
 
-//compile and deploy with req params
-// req.body.userAddress
-// req.body.chainID
-// const abi = contractFile.abi;
+    //compile and deploy with req params
+    // req.body.userAddress
+    // req.body.chainID
+    // const abi = contractFile.abi;
 
+    const tokenName = req.body.tokenName;
+    const tokenSymbol = req.body.tokenSymbol;
+    const tokenDecimal = req.body.tokenDecimal;
+    const tokenSupply = req.body.tokenSupply;
+    const uniSwapRouter = "0xD99D1c33F9fC3444f8101754aBC46c52416550D1";
 
-const newAddress = new Address({
-    user: req.body.userAddress,
-    token: "",// token address from deployment
-    chainID: req.body.chainID
-});
-newAddress.save()
-            .then(data => res.json(data))
-            .catch(err => console.log(err));
+    data = compiler('token');
+    (async () => {
+        address2 = await deployer(data, tokenName, tokenSymbol, tokenDecimal, tokenSupply, uniSwapRouter);
+        res.json(address2);
+    })()
+
+    const newAddress = new Address({
+        user: req.body.userAddress,
+        token: "",// token address from deployment
+        chainID: req.body.chainID
+    });
+
+    newAddress.save()
+        .then(data => res.json(data))
+        .catch(err => console.log(err));
+
 });
 
 router.post("/createPad", (req,res)=>{
 
-//compile and deploy with req params
-// req.body.userAddress
-// req.body.chainID
-// req.body.tokenAddress
+    //compile and deploy with req params
+    // req.body.userAddress
+    // req.body.chainID
+    // req.body.tokenAddress
 
-updateAddress = {};
-updateAddress.user = req.body.userAddress;
-updateAddress.token = req.body.tokenAddress;
-updateAddress.launchpad = ""; // launchpad address acquired from deployment
-updateAddress.chainID = req.body.chainID;
+    updateAddress = {};
+    updateAddress.user = req.body.userAddress;
+    updateAddress.token = req.body.tokenAddress;
+    updateAddress.launchpad = ""; // launchpad address acquired from deployment
+    updateAddress.chainID = req.body.chainID;
 
-Address.findOneAndUpdate(
-    { user: req.body.userAddress },
-    { $set: updateAddress },
-    { new: true }
-).then(profile => res.json(profile));
+    Address.findOneAndUpdate(
+        { user: req.body.userAddress },
+        { $set: updateAddress },
+        { new: true }
+    ).then(profile => res.json(profile));
 
 });
 
